cc06ec9045f125238637629aba22b1ea51484cc8
OlegMishko <oleg.mishko@gmail.com>
2022-09-18 16:37:48 +0300

    Update App.jsx
    очищены предыдущие файлы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,12 @@
-import user from 'data/user.json';
-import data from 'data/data.json';
-import friends from 'data/friends.json';
-import transactions from 'data/transactions.json';
 
-import { FriendList } from './FriendList/FriendList';
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { TransactionHistory } from './TransactionsHistory/TransactionHistory';
+
 
 import { MainPage } from 'common/MainPage.styled';
 
 export const App = () => {
   return (
     <MainPage>
-      <h2>Task 1 - User profile</h2>
-      <Profile user={user} />
-
-      <h2>Task 2 - Statistics</h2>
-      <Statistics data={data} />
-
-      <h2>Task 3 - FriendList</h2>
-      <FriendList friends={friends} />
-
-      <h2>Task 4 - TransactionHistory</h2>
-      <TransactionHistory transactions={transactions} />
+      
     </MainPage>
   );
 };
